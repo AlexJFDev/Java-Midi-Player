fd5fe48f4a45a39886114e6fe501dd74891563a6
Alex <alexjfuhr@gmail.com>
2021-12-04 14:47:23 -0500

    Added player file

---
+++ MiniMusicCmdLine.java
@@ -0,0 +1,51 @@
+import javax.sound.midi.*;
+
+public class MiniMusicCmdLine{
+
+    public static void main(String[] args){
+        MiniMusicCmdLine mini = new MiniMusicCmdLine();
+        if (args.length < 2) {
+            System.out.println("Don't forget the instrument and note args");
+        } else {
+            Integer instrument = Integer.parseInt(args[0]);
+            Integer note = Integer.parseInt(args[1]);
+            for(Integer i = 0; i < 128; i++){
+                for(Integer j = 63; j < 64; j++){
+                    System.out.println("Playing "+Integer.toString(i)+", "+Integer.toString(j));
+                    mini.play(i, j);
+                }
+                
+            }
+        }
+    }
+
+    public void play(int instrument, int note){
+
+        try {
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            MidiEvent event = null;
+
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
+
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, note, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128, 1, note, 100);
+            MidiEvent noteOff = new MidiEvent(b, 160);
+            track.add(noteOff);
+            player.setSequence(seq);
+            player.start();
+
+        } catch (Exception ex) {ex.printStackTrace();}
+    }
+}
